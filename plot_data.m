204972d53743ddd3b238074e6477699525dc99fa
jvgiordano <jgiordano100@gmail.com>
2019-05-21 15:05:04 +0200

    Updated plot_data to include title, legend, x and y labels, and clear data point markers Added photo of data

--- plot_data.m
+++ plot_data.m
@@ -9,20 +9,24 @@
 % Made by: Jonny Giordano
 % Data: May 21st, 2019
 
+%Choose and import data
 file = '01cr.set'; %Select file
 
 [data, labels] = extract_data(file); %Call extract data function
 
-data_mean = mean(data,2); %Find time averages for all electrodes
+%Process data
+data_mean = mean(data,2); %Find time averages for all electrodes, for each trial
 
-left = find(labels == 1);
-right = find(labels == 2);
+%Create indices for Left/Right saccade from label
+left = find(labels == 1); %Create array to denote when saccade is to the left from label array
+right = find(labels == 2); %Create array to denote when saccade is to the right
 
-plot(data_mean(1,left), data_mean(2, left), 'X'); %Plot electrode data first left saccade
+%Plot the data
+plot(data_mean(1,left), data_mean(2, left), 'X', 'MarkerSize', 12, 'LineWidth', 1.5); %Plot electrode data, left saccade
 hold on;
-plot(data_mean(1, right), data_mean(2, right), 'O'); %Plot electrode data for right saccade
-title({'Right versus Left Saccade','L_HEOG and R_HEOG'});
-xlabel('L_HEOG')
-ylabel('R_HEOG')
+plot(data_mean(1, right), data_mean(2, right), 'O', 'MarkerSize', 10, 'LineWidth', 1.5); %Plot electrode data, right saccade
+title({'Right versus Left Saccade','L-HEOG and R-HEOG'});
+xlabel('L-HEOG')
+ylabel('R-HEOG')
 legend('Left Saccade', 'Right Saccade')
 
