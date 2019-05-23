4f77485e9947ebc137544178d3f5ef9d09f04312
jvgiordano <jgiordano100@gmail.com>
2019-05-23 14:55:21 +0200

    Continued work on Decode, completed data pre-processing for logitic regression Finished compute_cost function  (non-regularized)

--- Decode.m
+++ Decode.m
@@ -9,29 +9,49 @@
 % Made by: Jonny Giordano
 % Date: May 21st, 2019
 
-%Choose Training File and Import
+% Break down of code
+% 1. Extract data
+% 2. Adjust Data Matrix, Add Intercept
+% 3. Initialize Theta
+% 4. Call Cost Function
+% 5. Gradient Descent
+% 6. Plot Decision Boundary
+% 7. Test on another data set
+%
+
+%% 1. Choose Training File and Import
 file = '01cr.set'; %Select file
 
-[data, labels] = extract_data(file); %Call extract data function
+[data_dirty, labels] = extract_data(file); %Call extract data function, labels denotes trial outcomes/condition
 
-%Process data, Add Intercept of '1'
-data_mean = mean(data,2); %Find time averages for all electrodes, for each trial
+%% 2. Process data, Add Intercept of '1', Adjust labels
+data_mean = mean(data_dirty,2); %Find time averages for all electrodes, for each trial
 data_mean = squeeze(data_mean); %Remove unnecessary dimension
 
 data_mean = transpose(data_mean); %Switch to vertical style
-[rows column] = size(data_mean); %Find dimensions of data
+[rows, column] = size(data_mean); %Find dimensions of data
 data_mean = [ones(rows, 1) data_mean]; %Add interncept of '1'
+data = data_mean;
+
+labels = labels-1; %Previously,  '2' = right, '1' = left
+                   %For our cost calculate, set '1' = right, '0' = left
+labels = transpose(labels); %Switch to verticle style
+
+%% 3. Initialize our Theta
+
+Theta = zeros(column+1, 1); %Initialize our Theta to a vector with number of data sources (electrodes) plus an intercept
+
+
+%% 4. Call our cost function
+
+[gradient, cost] = compute_cost(data, labels, Theta);
+
+
+%% 5. 
 
 
 
-%Initialize our Theta
-
-Theta = zeros(column+1, 1);
-
-%Develop our cost function
-
-cost = 0; %Set cost to 0
-gradient = zeros(size(Theta)); %Set gradient to zeros
+%% 6.
 
 
 
--- compute_cost.m
+++ compute_cost.m
@@ -11,8 +11,14 @@
 
 function [gradient, cost] = compute_cost(data, labels, Theta);
 
+cost = 0; %Initialize cost variable
+gradient = zeros(size(Theta)); %Creates vertical array of 0s with as many rows as Theta
+N = length(labels); %Find number of trials
 
+h = (1 + exp(-(data*Theta))).^(-1); %This computes the sigmoid of our data times our weights
+                                 % ie 'h' is our predicted value
 
-
+cost = (1/N) * sum(-labels.*log(h)-(1-labels).*log(1-h)); %Calculate the cost
+gradient = (1/N)*sum((h-labels).*data); %Calculate the gradient
 
 end
