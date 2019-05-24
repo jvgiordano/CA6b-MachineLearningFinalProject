d7722566abe88a36a9efb72ccd2042f04dfe989f
JudiKL <45490444+JudiKL@users.noreply.github.com>
2019-05-24 09:51:49 +0200

    Add files via upload

---
+++ plot_data_directiongaze.m
@@ -0,0 +1,13 @@
+% two_dmatrix is name of data matrix, 'output from extract eeg data' function
+% Find Indices of Left and Right Examples
+left = find(label==1); right = find(label == 2);
+% Plot Examples
+plot(two_dmatrix(left, 1), two_dmatrix(left, 2), 'k+','LineWidth', 2, ...
+'MarkerSize', 7);
+hold on
+plot(two_dmatrix(right, 1), two_dmatrix(right, 2), 'ko', 'MarkerFaceColor', 'y', ...
+'MarkerSize', 7);
+
+xlabel('left gaze direction')
+ylabel('right gaze direction')
+title('distribution of ocular electrodes mean voltage as a function of gaze direction')
